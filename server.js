4a1b183df9a71e6db579972ff63ed4603b9b1e2e
Steve Good <sgood@lanctr.com>
2017-11-01 15:32:14 -0700

    Added body parser

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
-
 const port = process.env.PORT || 3000
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
 
 app.get('/', function(req, res) {
   res.send("get pong!")
