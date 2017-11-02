4187caf341c5eda513551bd90f6e85d8223e1be8
Steve Good <sgood@lanctr.com>
2017-11-02 13:49:26 -0700

    Added release-it code and tag checking.

--- src/repo-service.js
+++ src/repo-service.js
@@ -1,5 +1,6 @@
 import Git from 'nodegit'
 import path from 'path'
+import shell from 'shelljs'
 
 const updateXwingData = async (temp_dir, tag) => {
   const username = process.env.USERNAME
@@ -10,53 +11,71 @@ const updateXwingData = async (temp_dir, tag) => {
 
   console.log(`Cloning https://${repo_url}`)
   const repo = await Git.Clone(gh_xdm_repo, temp_dir)
-  
   console.log(`Cloned https://${repo_url}`)
-  const submoduleNames = await repo.getSubmoduleNames()
   
-  if (submoduleNames.length === 0) return
+  const tags = await Git.Tag.list(repo)
+  const has_tag_already = tags.filter( t => t === tag ).length === 1
 
-  const submoduleName = submoduleNames[0]
+  if (has_tag_already) {
+    console.log(`This repo already has a tag named ${tag}. Skipping doing any other work...`)
+  } else {
+    const submoduleNames = await repo.getSubmoduleNames()
+    
+    if (submoduleNames.length === 0) return
   
-  console.log(`Looking up submodule ${submoduleName}`)
-  const submodule = await Git.Submodule.lookup(repo, submoduleName)
+    const submoduleName = submoduleNames[0]
+    
+    console.log(`Looking up submodule ${submoduleName}`)
+    const submodule = await Git.Submodule.lookup(repo, submoduleName)
+    
+    console.log(`Working with submodule ${submodule.name()}`)
+    await submodule.init(1)
+    await submodule.update(1, new Git.SubmoduleUpdateOptions())
   
-  console.log(`Working with submodule ${submodule.name()}`)
-  await submodule.init(1)
-  await submodule.update(1, new Git.SubmoduleUpdateOptions())
-
-  console.log("Submodule should be ready to work with now...")
-  const xd_repo = await submodule.open()
-
-  const xd_tag = await xd_repo.getTagByName(tag)
-  const hash = xd_tag.targetId().toString()
+    console.log("Submodule should be ready to work with now...")
+    const xd_repo = await submodule.open()
   
-  console.log(`Checking out hash ${hash}`)
-  await Git.Checkout.tree(xd_repo, hash)
-  xd_repo.setHeadDetached(hash)
-  console.log(`Checked out at ${tag} (${hash})`)
-
-  console.log('Refreshing the main repo index...')
-  const index = await repo.refreshIndex()
+    const xd_tag = await xd_repo.getTagByName(tag)
+    const hash = xd_tag.targetId().toString()
+    
+    console.log(`Checking out hash ${hash}`)
+    await Git.Checkout.tree(xd_repo, hash)
+    xd_repo.setHeadDetached(hash)
+    console.log(`Checked out at ${tag} (${hash})`)
   
-  console.log('Adding all changes to index')
-  await index.addByPath(path.join('.', 'xwing-data'))
-  await index.write()
-  const oidResult = await index.writeTree()
-  const head = await Git.Reference.nameToId(repo, 'HEAD')
-  const parent = await repo.getCommit(head)
+    console.log('Refreshing the main repo index...')
+    const index = await repo.refreshIndex()
+    
+    console.log('Adding all changes to index')
+    await index.addByPath(path.join('.', 'xwing-data'))
+    await index.write()
+    const oidResult = await index.writeTree()
+    const head = await Git.Reference.nameToId(repo, 'HEAD')
+    const parent = await repo.getCommit(head)
+    
+    const commit_msg = `Updating to xwing-data ${tag}`
+    console.log(`Adding commit with message "${commit_msg}"`)
+    const author = Git.Signature.now('Steve Good', 'sgood@lanctr.com')
+    const committer = Git.Signature.now('Steve Good', 'sgood@lanctr.com')
+    const commitId = await repo.createCommit('HEAD', author, committer, commit_msg, oidResult, [parent])
+    console.log(`New commit: ${commitId}`)
   
-  const commit_msg = `Updating to xwing-data ${tag}`
-  console.log(`Adding commit with message "${commit_msg}"`)
-  const author = Git.Signature.now('Steve Good', 'sgood@lanctr.com')
-  const committer = Git.Signature.now('Steve Good', 'sgood@lanctr.com')
-  const commitId = await repo.createCommit('HEAD', author, committer, commit_msg, oidResult, [parent])
-  console.log(`New commit: ${commitId}`)
-
-  // only push if the flag is true
-  if (push) {
-    // do the push
-    // do the release
+    // only push if the flag is true
+    if (push) {
+      console.log('Tagging and releasing...')
+      shell.cd(temp_dir)
+      const exec_opts = {
+        async: false,
+        silent: false
+      }
+      console.log('Having yarn install modules...')
+      shell.exec('yarn', exec_opts)
+  
+      console.log('Running release-it...')
+      shell.exec(`node_modules/.bin/release-it ${tag} --non-interactive`, exec_opts)
+      shell.cd('../')
+      console.log('Done!')
+    }
   }
 }
 
